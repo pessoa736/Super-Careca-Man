2952d8020113f7fe0c3c6dbadfcb0fadc81d31aa
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-04-19 21:50:24 -0300

    fix: index.html
    - index.html movido para o diretório root do projeto

--- game/src/canva.js
+++ game/src/canva.js
@@ -12,8 +12,9 @@ main.appendChild(canvas);
 function updateCanvas() {
     main.style.width = parseInt(window.innerWidth)*0.8 + "px"
     main.style.height =parseInt(window.innerHeight)*0.8 + "px"
-    canvas.width = main.offsetWidth*0.8;
-    canvas.height = main.offsetHeight*0.8;
+    main.style.justifyItems = "center"
+    canvas.width = main.offsetWidth*0.9;
+    canvas.height = main.offsetHeight*0.9;
 }
 
 export { canvas, ctx, updateCanvas};
