feb2a594d11b70085a4d52f96ae01a9e60408d14
Davi dos Santos Passos <69681778+pessoa736@users.noreply.github.com>
2025-04-22 14:59:33 -0300

    Update canva.js

--- docs/src/canva.js
+++ docs/src/canva.js
@@ -15,8 +15,8 @@ main.appendChild(canvas);
 function updateCanvas() {
     let aspect = 240/136;
         
-    main.style.width = parseInt(window.innerHeight*aspect)*0.8 + "px"
-    main.style.height =parseInt(window.innerHeight)*0.8 + "px"
+    main.style.width = parseInt(window.innerHeight*aspect)*0.7 + "px"
+    main.style.height =parseInt(window.innerHeight)*0.7 + "px"
     canvas.width = main.offsetWidth - 0.075*main.offsetWidth;
     canvas.height = main.offsetHeight - 0.15*main.offsetHeight;
 }
